144c3698c1f490424be33ff8a0b3669c30adf878
cesar.fraisseix <cesar.fraisseix@gmail.com>
2018-03-11 11:17:39 +0100

    Base du projet avec BDD connectée

---
+++ src/servlets/MainServlet.java
@@ -0,0 +1,28 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+
+public class MainServlet extends HttpServlet {
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher("/WEB-INF/view/test.jsp").forward(request, response);
+
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String message = "Transmission de variables : OK !";
+        request.setAttribute( "test", message );
+
+        String attribute = (String) request.getAttribute("test");
+        this.getServletContext().getRequestDispatcher("/WEB-INF/view/test.jsp").forward(request, response);
+
+
+    }
+}
